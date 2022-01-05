507877c9e08a15c3e490d4fc02a64029356519f5
Helen Cristhina A <hcristha@outlook.com>
2022-01-05 17:18:32 -0300

    script.js
    Como melhorar?

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function jsIcon(){
+function jsIcon0(img){
+    img.style.transform = "rotateX(180deg)";
+
     let answer0 = document.getElementsByClassName("js-answer0")[0];
-    
     answer0.style.display = "inline";
+}
+
+function jsIcon1(img){
+    img.style.transform = "rotateX(180deg)";
+
+    let answer1 = document.getElementsByClassName("js-answer1")[0];
+    answer1.style.display = "inline";
+}
+
+function jsIcon2(img){
+    img.style.transform = "rotateX(180deg)";
+
+    let answer2 = document.getElementsByClassName("js-answer2")[0];
+    answer2.style.display = "inline";
+}
+
+function jsIcon3(img){
+    img.style.transform = "rotateX(180deg)";
+
+    let answer3 = document.getElementsByClassName("js-answer3")[0];
+    answer3.style.display = "inline";
+}
+
+function jsIcon4(img){
+    img.style.transform = "rotateX(180deg)";
+    let answer4 = document.getElementsByClassName("js-answer4")[0];
+    answer4.style.display = "inline";
 }
